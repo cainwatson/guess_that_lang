496b77fb4300373779ca1d024599bb0e7d427fbf
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2020-11-28 14:51:57 -0600

    chore: install tentacat github client

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule GuessThatLang.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:tentacat, "~> 2.2"}
     ]
   end
 
