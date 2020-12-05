34ec8263649597a0d1b35bf6c469ff57133db787
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2020-12-04 20:21:42 -0600

    feat: add github rate limit logging

--- lib/guess_that_lang/code_searcher/github.ex
+++ lib/guess_that_lang/code_searcher/github.ex
@@ -1,4 +1,6 @@
 defmodule GuessThatLang.CodeSearcher.Github do
+  require Logger
+
   @topics [
     "app",
     "arduino"
@@ -21,6 +23,33 @@ defmodule GuessThatLang.CodeSearcher.Github do
     Tentacat.Client.new(%{access_token: access_token})
   end
 
+  defp check_rate_limit(%HTTPoison.Response{headers: headers} = response) do
+    {_, limit} = List.keyfind(headers, "X-RateLimit-Limit", 0)
+    {_, remaining} = List.keyfind(headers, "X-RateLimit-Remaining", 0)
+    {_, reset} = List.keyfind(headers, "X-RateLimit-Reset", 0)
+
+    {limit, _} = Integer.parse(limit)
+    {remaining, _} = Integer.parse(remaining)
+    {reset, _} = Integer.parse(reset)
+        reset = DateTime.from_unix!(reset)
+
+    half_limit = Integer.floor_div(limit, 2)
+    three_quarters_limit = Integer.floor_div(limit, 3)
+
+    cond do
+      remaining == 0 ->
+        Logger.error("Github rate limit reached! #{remaining}/#{limit} used. Resets at #{reset}")
+        {:error, {:rate_limit_reached, response}}
+
+      remaining in [half_limit, three_quarters_limit, 1] ->
+        Logger.warn("Github rate limit status: #{remaining}/#{limit} remaining. Resets at #{reset}")
+        :ok
+
+      true ->
+        :ok
+    end
+  end
+
   defp fetch_code(opts) do
     batch_size = Keyword.fetch!(config(), :batch_size)
     language = Keyword.fetch!(opts, :language)
@@ -31,9 +60,11 @@ defmodule GuessThatLang.CodeSearcher.Github do
       per_page: batch_size
     }
 
-    with {200, %{"items" => codes}, _response} <-
+    with {200, %{"items" => codes}, response} <-
            Tentacat.Search.code(client(), params, pagination: :none),
-         do: {:ok, codes}
+         :ok <- check_rate_limit(response) do
+      {:ok, codes}
+    end
   end
 
   defp fetch_content(%{} = code) do
@@ -41,8 +72,10 @@ defmodule GuessThatLang.CodeSearcher.Github do
     repo = code["repository"]["name"]
     path = code["path"]
 
-    with {200, %{"encoding" => "base64", "content" => content_base64}, _response} <-
+    with {200, %{"encoding" => "base64", "content" => content_base64}, response} <-
            Tentacat.Contents.find(client(), owner, repo, path),
-         do: {:ok, content_base64}
+         :ok <- check_rate_limit(response) do
+      {:ok, content_base64}
+    end
   end
 end
