ca9cfa5dbdc80c1fb363ef666cf4e956c431ff7e
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2020-12-14 19:07:49 -0600

    chore: uninstall database deps

--- mix.exs
+++ mix.exs
@@ -36,8 +36,6 @@ defmodule GuessThatLang.MixProject do
     [
       {:phoenix, "~> 1.5.6"},
       {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_live_view, "~> 0.14.6"},
       {:floki, ">= 0.27.0", only: :test},
       {:phoenix_html, "~> 2.11"},
