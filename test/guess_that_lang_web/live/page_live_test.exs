73d6c3f67abae8e73cf56b6509e4d9351274adee
Cain Watson <23223956+cainwatson@users.noreply.github.com>
2020-12-14 18:51:07 -0600

    test: render code snippet from CodeSearcher

--- test/guess_that_lang_web/live/page_live_test.exs
+++ test/guess_that_lang_web/live/page_live_test.exs
@@ -1,11 +1,29 @@
 defmodule GuessThatLangWeb.PageLiveTest do
-  use GuessThatLangWeb.ConnCase
+  use GuessThatLangWeb.ConnCase, async: true
 
   import Phoenix.LiveViewTest
+  import Mox
 
-  test "disconnected and connected render", %{conn: conn} do
+  # Make sure mocks are verified when the test exits
+  setup :verify_on_exit!
+
+  test "renders code snippet from CodeSearcher", %{conn: conn} do
+    GuessThatLang.CodeSearcher.MockSearcher
+    |> expect(:search, fn _opts ->
+      {:ok,
+       %{
+         content: "defmodule Hello do
+  def world do
+    IO.puts(\"Hello, world!\")
+  end
+end",
+         language: "Elixir"
+       }}
+    end)
+
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+
+    assert disconnected_html =~ "Guess That Lang!"
+    assert render(page_live) =~ "defmodule Hello do"
   end
 end
